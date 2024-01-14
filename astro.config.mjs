2cf540b6d72fa7d2f2004d14bb51288dcb205dc7
Tom de Bruin <tom.debruin@bbc.co.uk>
2024-01-14 16:37:22 +0000

    Update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://deadlyhifi.com",
+  site: "https://astronought.pages.dev/",
   integrations: [sitemap()],
 });
